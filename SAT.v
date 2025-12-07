d34f351bfcc73f297201164b15c1d68a43eb6a2d
Sofie Thomsen <202205754@post.au.dk>
2025-12-07 02:32:49 +0000

    solved optimizer1

--- SAT.v
+++ SAT.v
@@ -27,6 +27,14 @@ Proof.
   apply Nat.eqb_eq in H; rewrite H; reflexivity.
 Qed.
 
+(* EKSTRA HELPER FUNCTIONS *)
+Fixpoint fold {X Y: Type} (f : X->Y->Y) (l : list X) (b : Y)
+                         : Y :=
+  match l with
+  | nil => b
+  | h :: t => f h (fold f t b)
+  end.
+
 (* OPGAVER *)
 
 (* TODO - E1 *)
@@ -149,7 +157,18 @@ Fixpoint appendLists (l1 l2 :list id): list id :=
 
 Notation "l1 +++ l2" := (appendLists l1 l2) (at level 10).
 
-Fixpoint list_variables (p: form) (vl : list id) : list id :=
+Fixpoint list_variables (p: form) : list id :=
+  match p with
+  | Fvar x => [x]
+  | Fand f1 f2 => ((list_variables f1)) +++ (list_variables f2)
+  | For f1 f2 => ((list_variables f1)) +++ (list_variables f2)
+  | Fimplies f1 f2 => ((list_variables f1)) +++ (list_variables f2)
+  | Fnot f => (list_variables f)
+  | Ftrue => []
+  | Ffalse => []
+  end.
+
+(* Fixpoint list_variables (p: form) (vl : list id) : list id :=
   match p with
   | Fvar x => vl +++ [x]
   | Fand f1 f2 => (vl +++ (list_variables f1 vl)) +++ (list_variables f2 vl)
@@ -158,17 +177,17 @@ Fixpoint list_variables (p: form) (vl : list id) : list id :=
   | Fnot f => vl +++ (list_variables f vl)
   | Ftrue => vl
   | Ffalse => vl
-  end.
+  end. *)
 
-Example prop1_correct_var : list_variables prop1 [] = [(Id 1); (Id 2)].
+Example prop1_correct_var : list_variables prop1 = [(Id 1); (Id 2)].
 Proof.
   reflexivity.
 Qed.
-Example prop2_correct_var : list_variables prop2 [] = [(Id 2); (Id 1)].
+Example prop2_correct_var : list_variables prop2 = [(Id 2); (Id 1)].
 Proof.
   reflexivity.
 Qed.
-Example prop3_correct_var : list_variables prop3 [] = [(Id 1)].
+Example prop3_correct_var : list_variables prop3 = [(Id 1)].
 Proof.
   reflexivity.
 Qed.
@@ -200,7 +219,7 @@ Qed.
 
 
 Definition find_valuation (p : form ) : option valuation :=
-  match filter (fun (V: valuation) => interp V p) (generate_truthtable (list_variables p []) empty_valuation) with
+  match filter (fun (V: valuation) => interp V p) (generate_truthtable (list_variables p) empty_valuation) with
   | nil => None
   | h :: t => Some h
   end.
@@ -270,7 +289,7 @@ Proof.
   destruct (find_valuation p) eqn:EH.
   - unfold find_valuation in EH.
     (* DOKUMENTATION: https://stackoverflow.com/questions/78321778/is-is-possible-to-rename-a-coq-term *)
-    set (l:= generate_truthtable (list_variables p []) empty_valuation) in EH.
+    set (l:= generate_truthtable (list_variables p) empty_valuation) in EH.
     induction l as [| v' l' IHl'].
     + simpl in EH. discriminate.
     + destruct (interp v' p) eqn: Ev'.
@@ -282,16 +301,6 @@ Qed.
 (* EXTRA *)
 
 (* EXTRA -- Optimizer *)
-(* 
-Inductive form :  Type:=
-  | Fvar : id -> form  
-  | Ftrue : form
-  | Ffalse : form
-  | Fand : form -> form -> form
-  | For : form -> form -> form
-  | Fimplies : form -> form -> form
-  | Fnot : form -> form.
-*)
 
 Fixpoint Optimizer (p : form) : form :=
   match p with
@@ -305,16 +314,17 @@ Fixpoint Optimizer (p : form) : form :=
   (* DIREKTE OPTIMIZED *)
   | Fand f1 f2 =>
       match Optimizer f1, Optimizer f2 with
-      | (Fvar x), Ftrue => Fvar x
       | Ftrue, (Fvar x) => Fvar x
+      | (Fvar x), Ftrue => Fvar x
       | _, Ffalse => Ffalse
       | Ffalse, _ => Ffalse
       | _, _ => Fand (Optimizer f1) (Optimizer f2)
       end
-  | For f1 f2 =>
+  | For f1 f2 => 
+  (* For (Optimizer f1) (Optimizer f2) *)
       match Optimizer f1, Optimizer f2 with
-      | _, Ftrue => Ftrue
       | Ftrue, _ => Ftrue
+      | _, Ftrue => Ftrue
       | (Fvar x), Ffalse => Fvar x
       | Ffalse, (Fvar x) => Fvar x
       | _, _ => For (Optimizer f1) (Optimizer f2)
@@ -325,26 +335,52 @@ Compute (Optimizer (X F/\ Ftrue)).
 Compute (Optimizer (X F/\ Ffalse)).
 Compute (Optimizer ((Ftrue F\/ X) F-> (X F/\ Ftrue))).
 
+(* Ltac rwrt_refl :=
+  match goal with
+    H1: interp ?V ?P -> ?E
+    |- _ => rewrite H1
+  end. *) (* OBS - forstår ikke hvorfor den ikke fungerer *)
+
+Ltac rwrt_h1h2 h1 h2:=
+  rewrite h1 ; rewrite h2.
+
 Definition Optimizer_correct : forall (p:form) (v : valuation), 
     interp v p = interp v (Optimizer p).
 Proof.
-  intros p. induction p; intros v; try reflexivity.
-  - (* Fand *)
-    specialize IHp1 with v. specialize IHp2 with v.
-    destruct Optimizer 
-    assert (L: ((p1 F/\ p2) = (Optimizer (p1 F/\ p2))) -> (interp v (p1 F/\ p2) =
-            interp v (Optimizer (p1 F/\ p2)))).
-    {
-      intros. rewrite <- H. reflexivity.
-    }
-    apply L. destruct (Optimizer (Fand p1 p2)) eqn:E.
-    + admit.
-    + 
-    simpl.
-  - (* For *)
-  - (* Fimplies *)
-  - (* Fnot *)
-   unfold interp.
+  intros p v. induction p ; simpl ; try reflexivity.
+  - 
+    destruct (Optimizer p1) eqn: Ep1 ; 
+    try (
+          rwrt_h1h2 IHp1 IHp2 ; 
+          reflexivity
+        ) ;
+    simpl ; destruct (Optimizer p2) eqn:Ep2 ; try (rwrt_h1h2 IHp1 IHp2; reflexivity) ;
+    try (
+          rwrt_h1h2 IHp1 IHp2 ;
+          apply andb_true_r 
+        ) ;
+    try (
+          rwrt_h1h2 IHp1 IHp2 ;
+          apply andb_false_r 
+        ).
+  - 
+    destruct (Optimizer p1) eqn: Ep1 ;
+    try (
+          rwrt_h1h2 IHp1 IHp2 ; 
+          reflexivity
+        ) ;
+    simpl ; destruct (Optimizer p2) eqn:Ep2 ; try (rwrt_h1h2 IHp1 IHp2; reflexivity) ;
+    try (
+          rwrt_h1h2 IHp1 IHp2 ;
+          apply orb_true_r 
+        ) ;
+    try (
+          rwrt_h1h2 IHp1 IHp2 ;
+          apply orb_false_r 
+        ).
+  - rewrite IHp1. rewrite IHp2. reflexivity.
+  - rewrite IHp. reflexivity.  
+Qed. 
 
 Definition solver2 (p : form ) : bool :=
   match find_valuation (Optimizer p) with
@@ -355,40 +391,208 @@ Definition solver2 (p : form ) : bool :=
 Lemma solver2_sound : forall p, solver2 p = true -> satisfiable p.
 Proof.
   intros p H. 
-  unfold solver in H.
-  destruct (find_valuation p) eqn:EH.
-  - unfold find_valuation in EH.
-    (* DOKUMENTATION: https://stackoverflow.com/questions/78321778/is-is-possible-to-rename-a-coq-term *)
-    set (l:= generate_truthtable (list_variables p []) empty_valuation) in EH.
-    induction l as [| v' l' IHl'].
-    + simpl in EH. discriminate.
-    + destruct (interp v' p) eqn: Ev'.
-      * unfold satisfiable. exists v'. assumption.
-      * simpl in EH. rewrite Ev' in EH. apply IHl' in EH. assumption. 
-  - unfold solver2 in H. discriminate.
-Qed.  
+  unfold solver2 in H.
+  destruct (find_valuation (Optimizer p)) eqn:EH.
+  - unfold find_valuation in EH. 
+    unfold filter in EH. 
+    assert (L: forall x, interp x p = interp x (Optimizer p)).
+    {
+      apply Optimizer_correct.
+    }
+    set (tt := generate_truthtable (list_variables (Optimizer p)) empty_valuation) in EH.
+    induction tt as [| v' l' IHl'].
+    + discriminate.
+    + destruct (interp v' (Optimizer p)) eqn: Ev'.
+      * rewrite <- Optimizer_correct in Ev'. unfold satisfiable. exists v'. assumption.
+      * apply IHl'. assumption.
+  - discriminate.
+Qed.
 
+(* EXTRA -- Negation Normal Form *)
 
-Theorem optimize_0plus_sound: ∀ a,
-  aeval (optimize_0plus a) = aeval a.
-Proof.
-
-  (* | Fand (Fval x) Ftrue => Fval x
-  | Fand Ftrue (Fval x) => Fval x
-  | Fand _ Ffalse => Ffalse
-  | Fand Ffalse _ => Ffalse
-  | For _ Ftrue => Ftrue
-  | For Ftrue _ => Ftrue
-  | For (Fval x) Ffalse => Fval x
-  | For Ffalse (Fval x) => Fval x *)
-  (* SENDES VIDERE *)
-  | Fand f1 f2 => 
-  
+(* 
+  match p with
+  | Fvar x => V x
+  | Ftrue => true
+  | Ffalse => false
+  | Fand f1 f2 => andb (interp V f1) (interp V f2)
+  | For f1 f2 => orb (interp V f1) (interp V f2)
+  | Fimplies f1 f2 =>
+      match interp V f1 with
+      | true => interp V f2
+      | false => true
+      end
+  | Fnot f => negb (interp V f)
   end.
+
+
+   match p with
+  (* SAME *)
+  | Fvar x => Fvar x
+  | Ftrue => Ftrue
+  | Ffalse => Ffalse
+  (* SENDES VIDERE *)
+  | Fnot f => Fnot (Optimizer f)
+  | Fimplies f1 f2 => Fimplies (Optimizer f1) (Optimizer f2)
+  (* DIREKTE OPTIMIZED *)
+  | Fand f1 f2 =>
+      match Optimizer f1, Optimizer f2 with
+      | Ftrue, (Fvar x) => Fvar x
+      | (Fvar x), Ftrue => Fvar x
+      | _, Ffalse => Ffalse
+      | Ffalse, _ => Ffalse
+      | _, _ => Fand (Optimizer f1) (Optimizer f2)
+      end
+  | For f1 f2 => 
+  (* For (Optimizer f1) (Optimizer f2) *)
+      match Optimizer f1, Optimizer f2 with
+      | Ftrue, _ => Ftrue
+      | _, Ftrue => Ftrue
+      | (Fvar x), Ffalse => Fvar x
+      | Ffalse, (Fvar x) => Fvar x
+      | _, _ => For (Optimizer f1) (Optimizer f2)
+      end
+  end.
+*)
+Fixpoint Optimizer_NNF (p : form) : form :=
+  match p with
+  (* SAME *)
+  | Fvar x => Fvar x
+  | Ftrue => Ftrue
+  | Ffalse => Ffalse
+  (* SENDES VIDERE *)
+  | Fand f1 f2 => Fand (Optimizer_NNF f1) (Optimizer_NNF f2)
+  (* | Fand f1 f2 => Fand (Optimizer_NNF f1) (Optimizer_NNF f2) *)
+  | For f1 f2 => For (Optimizer_NNF f1) (Optimizer_NNF f2)
+  | Fimplies f1 f2 => Fimplies (Optimizer_NNF f1) (Optimizer_NNF f2)
+  (* DIREKTE OPTIMIZED *)
+  | Fnot f => 
+      match (Optimizer_NNF f) with
+      | Fvar x => Fnot (Fvar x)
+      | Ftrue => Ffalse
+      | Ffalse => Ftrue
+      (* | Fand f1 f2 => For ((Fnot f1)) ((Fnot f2))
+      | For f1 f2 => Fand ((Fnot f1)) ((Fnot f2))
+      | Fimplies f1 f2 => For ((Fnot f1)) (f2) *)
+      | Fand f1 f2 => For (Optimizer_NNF (Fnot f1)) (Optimizer_NNF (Fnot f2))
+      | For f1 f2 => Fand (Optimizer_NNF (Fnot f1)) (Optimizer_NNF (Fnot f2))
+      | Fimplies f1 f2 => For (Optimizer_NNF (Fnot f1)) (f2)
+      (* ORIGINALT KOM TIL AT SKRIVE:
+        | Fand f1 f2 => For (Optimizer_NNF (Fnot f1)) (Optimizer_NNF (Fnot f2))
+        | For f1 f2 => Fand (Optimizer_NNF (Fnot f1)) (Optimizer_NNF (Fnot f2))
+        | Fimplies f1 f2 => For (Optimizer_NNF (Fnot f1)) (Optimizer_NNF f2)
+      *)
+      | Fnot f' => f'
+      (* ORIGINALT KOM TIL AT SKRIVE: Fnot f' => Optimizer_NNF f' 
+         DOG IKKE NØDVENDIGT DA VI HAR MATCHED MED Optimizer_NNF f*)
+      end
+  end.
+
   
+  
+  (* DIREKTE OPTIMIZED *)
+  | Fand f1 f2 =>
+      match Optimizer f1, Optimizer f2 with
+      | Ftrue, (Fvar x) => Fvar x
+      | (Fvar x), Ftrue => Fvar x
+      | _, Ffalse => Ffalse
+      | Ffalse, _ => Ffalse
+      | _, _ => Fand (Optimizer f1) (Optimizer f2)
+      end
+  | For f1 f2 => 
+  (* For (Optimizer f1) (Optimizer f2) *)
+      match Optimizer f1, Optimizer f2 with
+      | Ftrue, _ => Ftrue
+      | _, Ftrue => Ftrue
+      | (Fvar x), Ffalse => Fvar x
+      | Ffalse, (Fvar x) => Fvar x
+      | _, _ => For (Optimizer f1) (Optimizer f2)
+      end
+  end.
+
+(* EXTRA -- Conjunctive Normal Form *)
+
+(* EXTRA -- Completeness *)
+(* 
+Definition valuation := id -> bool.
+Definition empty_valuation : valuation := fun x => false.
+Definition override (V : valuation ) (x : id) (b : bool ) : valuation :=
+  fun y => if beq_id x y then b else V y.
+*)
+Definition min_valuation (V : valuation) (p : form) : valuation :=
+  fold (fun (elem : id) (W : valuation) => override W elem (V elem)) (list_variables p) (empty_valuation). (* ret => valuation *)
+
+Lemma override_empty_valuation_eq : forall (V : valuation) (i : id) (b : bool),
+    (override empty_valuation i b) i = b.
+Proof.
+  intros. unfold override. Search beq_id. rewrite <-beq_id_refl. reflexivity.
+Qed.
+
+
+Lemma min_valuation_preserves_interp : forall (V : valuation) (p: form),
+    interp V p = interp (min_valuation V p) p.
+Proof.
+  intros V p. 
+  (* generalize dependent V.  *)
+  unfold min_valuation. unfold interp. 
+  set (l:= (list_variables p)).
+  induction (list_variables p) as [|IH].
+  (* -  simpl. reflexivity.
+  induction p; try reflexivity.
+  - simpl. unfold min_valuation. simpl. symmetry. apply override_empty_valuation_eq. 
+    assumption.
+  - unfold min_valuation. apply override_empty_valuation_eq.
+    (* unfold min_valuation. apply override_empty_valuation_eq with (). *)
+  simpl. 
+  admit.
+  (* Problemet er, at jeg har overrided n elementer ? så jeg laver induktion på det 
+    forkerte. Pt. laver jeg nemlig med det samme induktion på p *)
+  - simpl. admit.
+  - simpl.
+    assert (L1: interp V p1 = interp (min_valuation V (p1 F/\ p2)) p1).
+    {
+      unfold min_valuation. simpl. rewrite override_empty_valuation_eq with (i:= ).
+       simpl. rewrite IHp1. unfold interp. unfold min_valuation.
+    }
+  
+  unfold min_valuation. simpl.
+   simpl. *)
+
+  (* rewrite IHp1. rewrite IHp2. *)
+
+Admitted.
+
+Search list.
+
+Print existsb.
+
+Search In.
+
+Lemma contained : forall (V: valuation) (l : list id),
+    existsb (valuation) (eqb)
+
+
+    In 
+    (fun (elem : id) => if (In elem l)=true then V elem else false)
+    (generate_truthtable l empty_valuation).
+
+Lemma min_valuation_in_truthtable : forall (V: valuation) (p : form),
+    In (min_valuation V p) (generate_truthtable (list_variables p) (empty_valuation)).
+Proof.
+  intros V p. induction p.
+  -  simpl.
+Admitted.
+
+(* REDONE list_variables *)
+
+Lemma some_valuation_then_solver_true : forall p, exists V, 
+    find_valuation p = Some V -> solver p = true.
+Proof.
+Admitted.
 
 Lemma solver_complete : forall p,
-  satisfiable p -> solver p = true .
+    satisfiable p -> solver p = true.
 Proof.
-  intros p E. unfold satisfiable in E. destruct E. unfold solver. unfold find_valuation.
+  intros p H. inversion H.
+  unfold solver.
 Admitted.
